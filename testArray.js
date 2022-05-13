b134f4c958a1d93ba05857a48f884c7da050e68c
austinbhan <austinbhan@outlook.com>
2022-05-13 09:40:51 -0700

    Made Desired Objectives in HTML for Test

---
+++ testArray.js
@@ -0,0 +1,20 @@
+export const testArray = [ 
+    { number: 1, letter: 'a' },
+    { number: 2, letter: 'b' },
+    { number: 3, letter: 'c' }
+];
+
+export const lotrArray = [
+    { name: 'Frodo', race: 'hobbit', interests: 'breakfast' },
+    { name: 'Gimli', race: 'dwarf', interests: 'ale' },
+    { name: 'Aragorn', race: 'human', interests: 'sword fighty' },
+    { name: 'Legolas', race: 'elf', interests: 'going to Isengard' },
+    { name: 'Samwise', race: 'hobbit', interests: 'taters' }
+]
+
+export const techBroArray = [
+    { name: 'Larry Ellison', company: 'Oracle', city: 'Redwood Shores' },
+    { name: 'Steve Jobs' , company: 'Apple', city: 'Palo Alto' },
+    { name: 'Mark Benioff' , company: 'Salesforce' , city: 'San Francisco' },
+    { name: 'Mark Zuckerberg', company: 'Facebook', city: 'Palo Alto' }
+];
